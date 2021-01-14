4672d3e44d6caa318f5172312c02d2acb5b321f6
Katherine Lim <k4lim@ucsd.edu>
2021-01-14 14:21:51 -0800

    Lab 2 done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,5 +5,13 @@ $(document).ready(function() {
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
-            });
+    });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	// prevent page from reloading
+	e.preventDefault();
+	// this refers to the object that triggered event
+	$(this).css("background-color", "#7fff00");
 }
